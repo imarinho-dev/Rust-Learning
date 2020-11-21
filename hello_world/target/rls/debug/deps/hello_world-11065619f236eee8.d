f70c75172624dc9a29c3ceec69a14c9cfe90c25b
imarinho-dev <imarinho.dev@gmail.com>
2020-11-21 10:47:23 -0300

    added my fisrt hello world project in Rust language

---
+++ hello_world/target/rls/debug/deps/hello_world-11065619f236eee8.d
@@ -0,0 +1,5 @@
+/home/jr/Rust Projects/Rust-Learning/hello_world/target/rls/debug/deps/hello_world-11065619f236eee8.rmeta: src/main.rs
+
+/home/jr/Rust Projects/Rust-Learning/hello_world/target/rls/debug/deps/hello_world-11065619f236eee8.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/rls/debug/deps/hello_world-47911bf5da7ea210.d
@@ -0,0 +1,5 @@
+/home/jr/Rust Projects/Rust-Learning/hello_world/target/rls/debug/deps/hello_world-47911bf5da7ea210.rmeta: src/main.rs
+
+/home/jr/Rust Projects/Rust-Learning/hello_world/target/rls/debug/deps/hello_world-47911bf5da7ea210.d: src/main.rs
+
+src/main.rs:
